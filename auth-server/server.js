92f1755cb276bed9463bbbed29c9706eb20d752d
Nisi <denisa@parcellab.com>
2021-07-20 16:25:48 +0200

    add mongodb connection to server.js

--- auth-server/server.js
+++ auth-server/server.js
@@ -26,3 +26,19 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
+
+const db = require('./auth-server/models')
+const dbConfig = require('./auth-server/configs/db.config.js')
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Successfully connect to MongoDB.')
+  })
+  .catch(err => {
+    console.error('Connection error', err)
+    process.exit()
+  })
