3f82e67ebe683aae9c3b87047169d26ae506ea4a
Nisi <denisa@parcellab.com>
2021-07-23 15:03:16 +0200

    getAccount and saveAccount for user controller

--- auth-server/app/controllers/user.controller.js
+++ auth-server/app/controllers/user.controller.js
@@ -4,31 +4,82 @@ const Account = db.account
 
 const bcrypt = require('bcryptjs')
 
-exports.updateAccount = (req, res) => {
-  Account.findOne({
-    email: req.body.email
-  })
-    .exec((err, acc) => {
+exports.getAccounts = (req, res) => {
+  if (req.body.accountId) {
+    Account.findById(req.body.accountId).exec((err, acc) => {
       if (err) {
         res.status(500).send({ message: err })
         return
       }
 
       if (!acc) {
-        const account = new Account({
-          email: req.body.email,
-          pub_key: req.body.pub_key,
-          sec_key: bcrypt.hashSync(req.body.sec_key, 8)
-        })
-
-        account.save((err, user) => {
-          if (err) {
-            res.status(500).send({ message: err })
-            return
-          }
-
-          res.send({ message: 'Account was added successfully!' })
-        })
+        return res.status(404).send({ message: 'Account Not found.' })
       }
+
+      res.status(200).send({
+        id: acc._id,
+        pub_key: acc.pub_key,
+        sec_key: acc.sub_key
+      })
+    })
+  }
+
+  Account.find({
+    email: req.body.email
+  })
+    .exec((err, docs) => {
+      if (err) {
+        res.status(500).send({ message: err })
+        return
+      }
+
+      if (!docs) {
+        return res.status(404).send({ message: 'No Accounts found.' })
+      }
+
+      let result = docs.map(x => {
+        accountId = x._id,
+        pub_key = x.pub_key,
+        sub_key = x.sec_key
+      })
     })
 }
+  
+exports.saveAccount = (req, res) => {
+  const account = new Account({
+    email: req.body.email,
+    pub_key: req.body.pub_key,
+    sec_key: req.body.sec_key,
+  })
+
+  account.save((err, acc) => {
+    if (err) {
+      res.status(500).send({ message: err })
+      return
+    }
+
+    User.findOne({
+      email: req.body.email
+    })
+      .exec((err, user) => {
+        if (err) {
+          res.status(500).send({ message: err })
+          return
+        }
+  
+        if (!user) {
+          return res.status(404).send({ message: 'No User found. Could not update accounts[] for UserNo.' })
+        }
+        
+        const newAccount = {
+          accountId: acc._id,
+          pub_key: acc.pub_key,
+          sub_key: acc.sec_key
+        }
+        user.accounts.push(newAccount)
+      })
+
+    res.send({ message: 'Account was saved and User updated successfully!' })
+  })
+}
+  
