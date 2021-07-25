e918ef728835d6679ff154f6d9106baa4b90b9d3
Nisi <denisa@parcellab.com>
2021-07-25 15:41:48 +0200

    export whole class instead of single functions

--- stellar-wallet/src/services/auth.service.js
+++ stellar-wallet/src/services/auth.service.js
@@ -2,50 +2,52 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:8080/api/auth/'
 
-const signup = (username, email, password) => {
-  return axios
-    .post(API_URL + 'signup', {
-      username,
-      email,
-      password
-    })
-    .then((response) => {
-      return response
-    })
-    .catch((err) => {
-      return(err.response)
-    })
+class AuthService {
+  signup = (username, email, password) => {
+    return axios
+      .post(API_URL + 'signup', {
+        username,
+        email,
+        password
+      })
+      .then((response) => {
+        return response
+      })
+      .catch((err) => {
+        return(err.response)
+      })
+  }
+
+  login = (email, password) => {
+    return axios
+      .post(API_URL + 'signin', {
+        email: email,
+        password: password
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem('user', JSON.stringify(response.data))
+        }
+        
+        return response
+      })
+      .catch((err) => {
+        return(err.response)
+      })
+  }
+
+  logout = () => {
+    localStorage.removeItem('user')
+  }
+
+  updateCurrentUser = (user) => {
+    localStorage.removeItem('user')
+    localStorage.setItem('user', JSON.stringify(user))
+  }
+
+  getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem('user'))
+  }
 }
 
-const login = (email, password) => {
-  return axios
-    .post(API_URL + 'signin', {
-      email: email,
-      password: password
-    })
-    .then((response) => {
-      if (response.data.accessToken) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-      }
-      
-      return response
-    })
-    .catch((err) => {
-      return(err.response)
-    })
-}
-
-const logout = () => {
-  localStorage.removeItem('user')
-}
-
-const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem('user'))
-}
-
-export default {
-  signup,
-  login,
-  logout,
-  getCurrentUser
-}
+export default new AuthService()
