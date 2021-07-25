ae0b24692fc07d89832b5829203c0252dd0e55b0
Nisi <denisa@parcellab.com>
2021-07-25 15:42:34 +0200

    export whole class and extend functions

--- stellar-wallet/src/services/accountService.stellar.js
+++ stellar-wallet/src/services/accountService.stellar.js
@@ -1,37 +1,84 @@
+import axios from 'axios'
+import authHeader from './auth-header'
+
+const API_URL = 'http://localhost:8080/api/data/'
+
 const StellarSdk = require('stellar-sdk')
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org')
 // const fetch = require('node-fetch')
 
-// This is the test server
-// StellarSdk.Network.useTestNetwork()
-
-// And this is the productive server
-// StellarSdk.Network.usePublicNetwork()
-
 // Again…test server
-// const server = new StellarSdk.Server('https://horizon-testnet.stellar.org')
 
 // And productive server
 // const server = new StellarSdk.Server('https://horizon.stellar.org')
 
 // create a completely new and unique pair of keys
 // see more about KeyPair objects: https://stellar.github.io/js-stellar-sdk/Keypair.html
-const createKeypair = () => {
-  const pair = StellarSdk.Keypair.random()
-  const pubKey = pair.publicKey()
-  const secKey = pair.secret()
-  return {
-    pub_key: pubKey,
-    sec_key: secKey
+
+class AccountService {  
+  saveKeypair = (email, pub_key, sec_key) => {
+    return axios
+      .post(API_URL + 'accounts', {
+        email: email,
+        pub_key: pub_key,
+        sec_key: sec_key
+      }, { headers: authHeader() })
+      .then((response) => {
+        return response
+      })
+      .catch((err) => {
+        return (err.response)
+      })
   }
-}
 
-// the JS SDK uses promises for most actions, such as retrieving an account
-// const account = await server.loadAccount(pair.publicKey());
-// console.log("Balances for account: " + pair.publicKey());
-// account.balances.forEach(function (balance) {
-//   console.log("Type:", balance.asset_type, ", Balance:", balance.balance);
-// });
+  createKeypair = (email) => {
+    const pair = StellarSdk.Keypair.random()
+    const pubKey = pair.publicKey()
+    const secKey = pair.secret()
+    const account = { email: email, pub_key: pubKey, sec_key: secKey }
+    return account
+  }
 
-export default {
-  createKeypair
-}
+  getKeypairByEmail = (email) => {
+    return axios
+      .get(API_URL + 'accounts', {
+        email: email,
+      }, { headers: authHeader() })
+      .then((response) => {
+        return response
+      })
+      .catch((err) => {
+        return (err.response)
+      })
+  }
+
+  getKeypairById = (accountId) => {
+    return axios
+      .get(API_URL + 'accounts', {
+        accountId: accountId
+      }, { headers: authHeader() })
+      .then((response) => {
+        return response
+      })
+      .catch((err) => {
+        return (err.response)
+      })
+  }
+
+  getBalance = (pub_key) => {
+    return server.loadAccount(pub_key)
+      .then((response) => {
+        const res = [response.account_id, response.balances]
+        return res
+      })
+      .catch((err) => {
+        return (err)
+      })
+  }
+
+  fundNewAccount = (pub_key) => {
+    return axios
+      .get(`https://friendbot.stellar.org?addr=${pub_key}`)
+  }}
+
+  export default new AccountService()
