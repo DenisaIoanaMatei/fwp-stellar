a882a55e0da5ddafd332ae111152431f087ae175
Nisi <denisa@parcellab.com>
2021-08-01 19:13:57 +0200

    import Modal for showing errors

--- stellar-wallet/src/components/Login.js
+++ stellar-wallet/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 import AuthService from '../services/auth.service'
 import { useHistory } from 'react-router-dom'
 import '../stylesheets/login.css'
